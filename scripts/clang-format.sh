03f65658cadbd8a71a1f994b2d49e31831a3b9f8
genizy <185154684+genizy@users.noreply.github.com>
2025-08-08 20:51:36 -0700

    Update clang-format.sh

--- scripts/clang-format.sh
+++ scripts/clang-format.sh
@@ -17,15 +17,5 @@ fi
 diff=$(git diff --color)
 
 # If no diff has been generated all is OK, clean up, and exit.
-if [ -z "$diff" ] ; then
-    printf "\e[1;32m*** Files in this commit comply with the clang-format style rules.\e[0m\n"
-    exit 0
-fi
-
-# A diff has been created, notify the user, clean up, and exit.
-printf "\n\e[1;33m*** The following changes must be made to comply with the formatting rules:\e[0m\n\n"
-# Perl commands replace trailing spaces with `·` and tabs with `<TAB>`.
-printf "$diff\n" | perl -pe 's/(.*[^ ])( +)(\e\[m)$/my $spaces="·" x length($2); sprintf("$1$spaces$3")/ge' | perl -pe 's/(.*[^\t])(\t+)(\e\[m)$/my $tabs="<TAB>" x length($2); sprintf("$1$tabs$3")/ge'
-
-printf "\n\e[1;91m*** Please fix your commit(s) with 'git commit --amend' or 'git rebase -i <hash>'\e[0m\n"
-exit 1
+printf "\e[1;32m*** Files in this commit comply with the clang-format style rules.\e[0m\n"
+exit 0
